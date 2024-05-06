e223a264573f89d8f4e7f6545ac17ea189a22914
kibet <kmishmael@gmail.com>
2024-05-06 23:28:15 +0300

    chore: ui adjustments

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,16 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary": "#008ecc",
+				"light-grey": "#b5b5b5",
+				"dark-grey": "#919191",
+				"primary2": "#f3f9fb",
+				"secondary": "#f5f5f5",
+				"primary-dark":"rgb(31, 41, 55)"
+			}
+		},
 	},
 	plugins: [],
 }
